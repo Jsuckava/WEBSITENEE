a236be41dc9372f78e5f68314121b72261274cde
Jsuckava <147977927+Jsuckava@users.noreply.github.com>
2025-05-11 00:10:58 +0800

    Add files via upload

--- project-root/checkData.js
+++ project-root/checkData.js
@@ -1,5 +1,5 @@
 const sql = require('msnodesqlv8');
-const connectionString = "server=MSI\\MSSQLPATSV;Database=LEIFOODHUBdb;Trusted_Connection=Yes;Encrypt=yes;TrustServerCertificate=yes;Driver={ODBC Driver 17 for SQL Server}";
+const connectionString = "server=MSI\\MSSQLPATSV;Database=lei_foodhubDb;Trusted_Connection=Yes;Encrypt=yes;TrustServerCertificate=yes;Driver={ODBC Driver 17 for SQL Server}";
 
 const query = 'SELECT * FROM LF_Users';
 
